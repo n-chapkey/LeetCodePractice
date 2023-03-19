850b4cb7cec0290484a3477d17917f01da35fd3a
Nicholas Chapkey <nchapkey@gmail.com>
2023-03-19 17:09:25 -0400

    Time: 75 ms (17.00%), Space: 44 MB (5.33%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let j = 0;
+    
+    if(s.length < 1) return true;
+    
+    for(let i in t){
+        if(s[j] === t[i]){
+            j = j + 1;
+        }
+        if(j == s.length){
+            return true;
+        }
+    }
+    return false;
+};
